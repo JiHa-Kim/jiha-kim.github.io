3abf87ecb15951974c48d8afff922a2fe072a3e1
Ji-Ha Kim <jihakim@outlook.com>
2026-03-16 13:12:27 +0000

    fix(test): remove redundant html-proofer cache to resolve build bottleneck

--- tools/test.sh
+++ tools/test.sh
@@ -65,8 +65,7 @@ main() {
     # test
     bundle exec htmlproofer "$SITE_DIR" \
       --disable-external \
-      --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/" \
-      --cache '{"timeframe": {"external": "1d", "internal": "1d"}}'
+      --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
   fi
 }
 
