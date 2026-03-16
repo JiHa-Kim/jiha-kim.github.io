e52281ab6e1efdf9e6c311723a4b77b61d2858e1
Ji-Ha Kim <jihakim@outlook.com>
2026-03-16 12:32:27 +0000

    perf(build): migrate site variables to Ruby and implement global card caching

--- _plugins/site_vars_optimizer.rb
+++ _plugins/site_vars_optimizer.rb
@@ -8,7 +8,6 @@ module Jekyll
 
     def generate(site)
       # 1. Pre-calculate Site Origin Type
-      # Mirroring logic from _includes/origin-type.html
       origin_type = 'cors'
       if site.config.dig('assets', 'self_host', 'enabled')
         env = site.config.dig('assets', 'self_host', 'env')
@@ -18,11 +17,17 @@ module Jekyll
       end
       site.data['origin_type'] = origin_type
 
-      # 2. Pre-calculate Language for all documents
-      # Mirroring logic from _includes/lang.html
+      # 2. Pre-calculate Site Preference Mode
+      site.data['prefer_mode'] = ""
+      if site.config['theme_mode']
+        site.data['prefer_mode'] = %(data-mode="#{site.config['theme_mode']}")
+      end
+
+      # 3. Pre-calculate Document-specific variables
       site.each_site_file do |item|
         next unless item.respond_to?(:data)
         
+        # Language detection
         page_lang = item.data['lang']
         if page_lang && site.data.dig('locales', page_lang)
           item.data['lang'] = page_lang
@@ -30,6 +35,11 @@ module Jekyll
           item.data['lang'] = site.config['lang']
         else
           item.data['lang'] = 'en'
+        end
+
+        # Header detection for TOC/Math (avoiding expensive contains checks in Liquid)
+        if item.respond_to?(:content) && item.content
+          item.data['has_h2_h3'] = item.content.include?('<h2') || item.content.include?('<h3')
         end
       end
     end
